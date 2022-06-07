89a0aa9a28df29fa7b1543d6274232cfc257bfc1
Aswin Ramesh <cs19b007@smail.iitm.ac.in>
2022-06-07 16:25:41 +0530

    test2

---
+++ test2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from staslib import stas
+
+if __name__ == "__main__":
+    cid = {
+        'transport': 'rdma',
+        'traddr': '10.197.155.169',
+        'subsysnqn': 'nqn.1988-11.com.dell:SFSS:2:20220208134025e8',
+        'trsvcid': '4420',
+        'host-traddr': '10.197.155.35',
+        'host-iface': 'wlp0s20f3'
+    }
+
+    tid = stas.TransportId(cid)
+
+    print(str(tid))
