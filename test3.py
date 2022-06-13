23eb79cca0374306badedda950cd1c2df995a0de
Aswin Ramesh <cs19b007@smail.iitm.ac.in>
2022-06-13 15:34:22 +0530

    a

---
+++ test3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from staslib import defs
+
+print(defs.VERSION)
+print(defs.LICENSE)
+print(defs.PROJECT_NAME)
+print(defs.STAC_DESCRIPTION)
+print(defs.STAC_ACRONYM)
+print(defs.STAF_DESCRIPTION)
+print(defs.STAF_ACRONYM)
+print(defs.STAFD_PROCNAME)
+print(defs.STAFD_DBUS_NAME)
+print(defs.STAFD_DBUS_PATH)
+print(defs.STAFD_EXECUTABLE)
+print(defs.STAFD_CONFIG_FILE)
