31336e31af201a28fbc1a6966cabc18414bc81e4
Aswin Ramesh <cs19b007@smail.iitm.ac.in>
2022-06-16 15:45:36 +0530

    a

--- stafd_copy.py
+++ stafd_copy.py
@@ -567,9 +567,9 @@ class Staf(stas.Service):
         controllers = stas.remove_blacklisted(configured_ctrl_list + discovered_ctrl_list + referral_ctrl_list)
         controllers = stas.remove_invalid_addresses(controllers)
 
-        for controller in controllers : 
-            if 'host-iface' in controller.keys() : 
-                controller.pop("host-iface")
+        # for controller in controllers : 
+        #     if 'host-iface' in controller.keys() : 
+        #         controller.pop("host-iface")
 
         new_controller_ids = {stas.TransportId(controller) for controller in controllers}
         cur_controller_ids = set(self._controllers.keys())
